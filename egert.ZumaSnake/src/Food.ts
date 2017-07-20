943a63d6d7a27101804167465375938575e29226
Printempsz <zhl19961214@vip.qq.com>
2017-07-20 11:38:57 +0800

    diyi!!!!!

---
+++ egert.ZumaSnake/src/Food.ts
@@ -0,0 +1,45 @@
+class Food extends egret.Sprite{
+    public color: Color;
+    public intake: number;
+    public FoodRadius: number;
+    public food: egret.Shape;
+    public constructor() {
+        super();
+    }
+    //随机生成食物
+    public GetRandomFood(r:number) {
+        this.food = new egret.Shape();
+        this.color = new Color();
+        this.intake = Math.round(Math.random() * 4);
+        this.FoodRadius = 0.2 * r * this.intake;
+        this.food.graphics.beginFill(this.color.origin);
+        this.food.graphics.drawCircle(0,0,this.FoodRadius);
+        this.food.graphics.endFill();
+        this.x = Math.random() * 1920;
+        this.y = Math.random() * 1080;
+        this.addChild(this.food);
+    }
+    //爆屎
+    public CreateAccelerate(x:number,y:number,r:number,color:Color){
+        this.food = new egret.Shape();
+        this.FoodRadius = r * 0.1 * Math.round(Math.random() * 4);
+        this.food.graphics.beginFill(color.origin);
+        this.food.graphics.drawCircle(0,0,this.FoodRadius);
+        this.food.graphics.endFill();
+        this.x = x;
+        this.y = y;
+        this.addChild(this.food);
+    }
+        //爆尸体
+    public GetBigFood(x:number,y:number,r:number,color:color){
+            this.food = new egret.Shape();
+            this.intake = 5;
+            this.FoodRadius = 0.2 * r * this.intake;
+            this.food.graphics.beginFill(color.origin);
+            this.food.graphics.drawCircle(0,0,this.FoodRadius);
+            this.food.graphics.endFill();
+            this.x = x;
+            this.y = y;
+            this.addChild(this.food);
+    }
+}
