00fed72b73364308ef075014083b355a8a2db4b3
Chengziqian <18663104463@163.com>
2017-07-21 10:04:27 +0800

    big

--- egert.ZumaSnake/src/Color.ts
+++ egert.ZumaSnake/src/Color.ts
@@ -4,10 +4,10 @@ class Color {
      * [紫，红，蓝，绿，黄，墨绿，橙]
      * [Purple, Red, Blue, Green, Yellow, Teal, Deep Orange]
      */
-    private OriginColor: number[] = 
+    public OriginColor: number[] = 
     [0x7B1FA2, 0xB71C1C, 0x1976D2, 0x2E7D32, 0xF9A825, 0x00897B, 0xF4511E];
 
-    private BrightColor: number[] =
+    public BrightColor: number[] =
     [0x9C27B0, 0xD32F2F, 0x2196F3, 0x43A047, 0xFBC02D, 0x26A69A, 0xFF7043];
 
     public Origin: number;
